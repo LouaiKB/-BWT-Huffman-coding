20479cd863b154d86ffc0b59a7a62c0f386f6966
LouaiKB <loukb1998@gmail.com>
2021-02-23 12:15:20 +0100

    add the leaf checker in process

--- tree_huffman.py
+++ tree_huffman.py
@@ -19,6 +19,7 @@ class TreeNode:
         self.right_child = None
         self.left_child = None
         self.data = data
+        self.character = ''
 
     def __str__(self) -> str:
 
@@ -30,8 +31,10 @@ class TreeNode:
         if self.right_child is None and self.left_child is None:
             return str(self.data)
 
-        return "(%s : %s) : %s" % (str(self.left_child),
+        return "(%s [%s]: %s [%s]) : %s" % (str(self.left_child),
+                                   self.character,
                                    str(self.right_child),
+                                   self.character,
                                    str(self.data))
 
     
@@ -57,3 +60,53 @@ class Tree:
             return 1
         
         return self.number_of_leafs(node.left_child) + self.number_of_leafs(node.right_child)
+
+    @staticmethod
+    def leaf_checker(root, frequency):
+        if root is None:
+            raise Exception('Node is empty')
+
+        queue = [root]
+        leafs = []
+
+        while len(queue) > 0:
+            node = queue.pop(0)
+            if node.left_child is not None:
+                queue.append(node.left_child)
+
+            if node.right_child is not None:
+                queue.append(node.right_child)
+            
+            elif node.left_child is None and node.right_child is None:
+                try:
+                    if frequency['A'] == node.data:
+                        node.character += 'A'
+                        frequency.pop('A')
+
+                    elif frequency['C'] == node.data:
+                        node.character += 'C'
+                        frequency.pop('C')
+
+                    elif frequency['G'] == node.data:
+                        node.character += 'G'
+                        frequency.pop('G')
+                    
+                    elif frequency['T'] == node.data:
+                        node.character += 'T'
+                        frequency.pop('T')
+                    
+                    elif frequency['N'] == node.data:
+                        node.character += 'N'
+                        frequency.pop('N')
+                    
+                    elif frequency['$'] == node.data:
+                        node.character = '$'
+                        frequency.pop('$')
+                except KeyError:
+                    pass
+
+        return leafs
+        
+        
+        
+
