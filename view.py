f18e604aaf6a793df2b5517ba675edb2689c3d8a
LouaiKB <loukb1998@gmail.com>
2021-03-07 22:48:15 +0100

    start ttk

--- view.py
+++ view.py
@@ -6,6 +6,15 @@ Author: Louai KB
 """
 
 from tkinter import Tk
+from tkinter import ttk
+
+root = Tk()
+
+root.title('test modern GUI!')
+
+button_ttk = ttk.Button(root, text='new ttk button').pack()
+
+root.mainloop()
 
 class View(Tk):
     pass
