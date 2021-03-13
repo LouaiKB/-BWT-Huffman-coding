513fb7487f90c160b323f87a5a39d2912ee96b0f
LouaiKB <loukb1998@gmail.com>
2021-03-13 02:53:20 +0100

    updating view

--- view.py
+++ view.py
@@ -1,14 +1,19 @@
 # coding ~utf-8
+
 """
 
 Author: Louai KB
 
 """
+import json
 import os
 import tkinter as tk
 from tkinter import ttk
-from tkinter.filedialog import askopenfile, asksaveasfile
+from tkinter.filedialog import askopenfile
+from tkinter.filedialog import asksaveasfile
+from tkinter.messagebox import askquestion
 from tkinter.messagebox import showinfo
+from tkinter.messagebox import showerror
 from PIL import Image
 from PIL import ImageTk
 from Bio import SeqIO
@@ -47,17 +52,28 @@ class View(tk.Tk):
     def setup_main_interface(self) -> None:
          PlaceImage.place_image(self)
 
+    def top_level_windows(self):
+        global top
+        global next_button
+        global next_button_text
+
+        top = tk.Toplevel()
+        PlaceImage.place_image(top)
+        next_button_text = tk.StringVar()
+        next_button = tk.Button(top, textvariable=next_button_text, bg='#53a6ba', fg='white',
+                               font='Raleway', height=2, width=15, bd=0.3)
+
+        next_button_text.set('next')
+        next_button.place(x=320, y=400)
+
+
     def create_buttons(self) -> None:
+
         for key, value in self.button_dict.items():
             button_text = tk.StringVar()
-            button = tk.Button(self,
-                               textvariable=button_text,
-                               bg='#53a6ba',
-                               fg='white',
-                               font='Raleway',
-                               height=2,
-                               width=15,
-                               bd=0.3)
+            button = tk.Button(self, textvariable=button_text, bg='#53a6ba', fg='white',
+                               font='Raleway', height=2, width=15, bd=0.3)
+
             button_text.set(key)
             button.place(x=value[0], y=value[1])
 
@@ -69,95 +85,184 @@ class View(tk.Tk):
                 button.configure(command=self.compression)
 
             elif key == 'Decompress':
-                pass
+                button.configure(command=lambda:"decompress")
 
             elif key == 'Full compression':
-                pass
+                button.configure(command=lambda:"full compression")
 
             elif key == 'Bwt encryption':
-                pass
+                button.configure(command=self.bwt_encryption)
 
             elif key == 'Bwt decrytpion':
-                pass
+                button.configure(command=lambda:"Bwt decryption")
 
             elif key == 'Full decompression':
-                pass
+                button.configure(command=lambda:"full decompression")
 
-
-        
-    def compression(self):
-        global results
-        global top
-        global next_button
-        global next_button_text
-
+    def open_and_get_size(self):
         file = askopenfile(parent=self, mode='r', title='choose a file', 
-                          filetypes=(('Text files', '*.txt'), ('Fasta file', '*.fasta')))
-        
-        top = tk.Toplevel()
-        PlaceImage.place_image(top)
-
+                filetypes=(('Text files', '*.txt'), ('Fasta file', '*.fasta')))
         self.controller = Controller(file.name)
-        self.controller.get_sequence_from_file()
-        results = self.controller.compression_process_without_bwt(self.controller.sequence)
-        next_button_text = tk.StringVar()
-        next_button = tk.Button(top, 
-                               textvariable=next_button_text,
-                               bg='#53a6ba',
-                               fg='white',
-                               font='Raleway',
-                               height=2,
-                               width=15,
-                               bd=0.3)
-        next_button_text.set('next')
-        next_button.place(x=320, y=400)
-        self.insert_in_text_box()
-    
-    def delete_text_box(self):
-        unicode = results.pop(first_key)
-        text_box.delete(1.0, 'end')
-        if len(results) != 0:
-            self.insert_in_text_box()
+        size_of_original_file = os.path.getsize(file.name)
+
+        return (file, size_of_original_file)
+
+    def save_file(self, additionnal_data=None, sequence=None):
+
+        if additionnal_data:
+
+            try:
+                file_to_save = asksaveasfile(initialdir=os.getcwd(), title="Select File", mode='w', defaultextension='txt')
+                with open(file_to_save.name, 'w', encoding='utf8') as f, open(file_to_save.name[:-4] + '_json_file.json', 'w') as jsn:
+                    f.write(unicode)
+                    json.dump(additionnal_data, jsn)
+
+                size_of_compressed_file = os.path.getsize(file_to_save.name)
+                showinfo('Successful message', 'File saved successfully!\nThe size of the original file is: %s\nThe size of the compressed file is: %s'
+                        %(str(size_of_original_file), str(size_of_compressed_file)))
+
+                top.destroy()
+
+            except AttributeError:
+                showerror('Error file!', 'Please enter a file')
+        
         else:
-            next_button_text.set('Save')
             file_to_save = asksaveasfile(initialdir=os.getcwd(), title="Select File", mode='w', defaultextension='txt')
             with open(file_to_save.name, 'w', encoding='utf8') as f:
-                f.write(unicode)
+                f.write(sequence)
             showinfo('Successful message', 'File saved successfully!')
             top.destroy()
 
-    def insert_in_text_box(self):
-        global text_box
-        global first_key
-        first_key = list(results)[0]
+
+    def compression(self):
+        global results
+        global binary_co
+        global encoder
+        global unicode
+        global size_of_original_file
+
+        try:
+            open_file = self.open_and_get_size()
+            size_of_original_file = open_file[1]
+
+            self.controller.get_sequence_from_file()
+            self.top_level_windows()
+
+            results = self.controller.compression_process_without_bwt(self.controller.sequence)
+            encoder = results.pop('encoder')
+
+            unicode = results[list(results)[-1]]
+            binary_co = results[list(results)[1]]
+        
+            self.compression_helper()
+        
+        except:
+
+            showerror('Error file!', 'Please enter the right file')
+
+    def insert_in_text_box(self, inserted_object):
+        global text_box         
         text_box = tk.Text(top, height=10, width=70)
-        text_box.insert('end', first_key.replace('_', ' ') + '\n' + results[first_key] + '\n')
+        text_box.insert('end', inserted_object)
         text_box.tag_configure('center', justify='center')
         text_box.tag_add('center', 1.0, 'end')
         text_box.place(x=120, y=185)
-        next_button.configure(command=self.delete_text_box)
+            
+    def compression_helper(self):
+        global first_key
+
+        try:
+            first_key = list(results)[0]
+            inserted_object = first_key + '\n' + results[first_key] + '\n'
+            self.insert_in_text_box(inserted_object)
+            next_button.configure(command=self.delete_text_box)
+        
+        except IndexError:
+            self.save_patterns()
+
+    def delete_text_box(self):
+        text_box.delete(1.0, 'end')
+
+        if len(results) != 0:
+            results.pop(first_key)
+            self.compression_helper()
+
+        else:
+            additionnal_data = {
+                'encoder': encoder, 
+                'binary_seq': binary_co
+            }
+            self.save_file(additionnal_data)
+
+    def save_patterns(self):
+        next_button_text.set('Save')
+        text_box.insert('end', '\nProcess completed whould you like to save the sequence?')
+        text_box.tag_configure('center', justify='center')
+        text_box.tag_add('center', 1.0, 'end')
+        next_button.place_configure(x=60, y=400)
+        
+        button_q = tk.Button(top, text='Quit', bg='#d07b1a', fg='white',
+                            font='Raleway', height=2, width=15, bd=0.3, command=top.quit)
+        
+        button_q.place(x=590, y=400)
+
+    
+    def bwt_encryption(self):
+        global bwt_matrix
+        global gen
+        global bwt_sequence
+
+        self.open_and_get_size()
+        self.controller.get_sequence_from_file()
+        
+        ask_quest = askquestion('Bwt encryption', 'Would you like to proceed the step by step Burrows Wheeler Transform?')
+        results_bwt = self.controller.bwt_encryption_step_by_step()
+        bwt_matrix = results_bwt[0]
+        bwt_sequence = results_bwt[1]
+
+        if ask_quest == 'yes':
+            gen = 'Step 1: The matrix construction '
+            self.top_level_windows()
+            self.insert_in_text_box(gen)
+            next_button.configure(command=lambda:self.get_next(gen))
+            next_button.bind('<Enter>', lambda:self.get_next(gen))
+        
+        else:
+            self.top_level_windows()
+            self.insert_in_text_box(bwt_sequence)
+            self.save_patterns()
+            next_button.configure(command=lambda: self.save_file(sequence=bwt_sequence))
+
+    
+    def get_next(self, gen):
+
+        try:
+            gen = ''
+            gen += text_box.get("1.0", "end") + next(bwt_matrix)
+            self.insert_in_text_box(gen)
+        
+        except StopIteration:
+
+            bwt = self.controller.bwt_encryption_step_by_step()
+            sorted_matrix = ''
+            for i in sorted(bwt[0]):
+                sorted_matrix += str(i) + '\n'
+            
+            gen = 'Step 2: The sort of the matrix\nThe BWT sequence is presented in the last column\n' + sorted_matrix + '\n\nBwt Sequence: ' + bwt_sequence
+            self.insert_in_text_box(gen)
+
+            self.save_patterns()
+            next_button.configure(command=lambda:self.save_file(sequence=bwt_sequence))
+
+            
+
+
 
     def main_loop(self):
         self.mainloop()
 
-# def open_fasta_file():
-#     compression_text.set('loading...')
-#     file = askopenfile(parent=root, mode='r', title='Choose genome file')
-#     if file:
-#         #sequence = SeqIO.read(file, 'fasta').seq + '$'
-#         text_box = tk.Text(root, height=100, width=50, padx=20, pady=10)
-#         for i in Bwt.bwt_construction_matrix('ATCATCATATCATGCA$'):
-#             text_box.insert('end', i + '\n')
-#             text_box.tag_configure('center', justify='center')
-#             text_box.tag_add('center', 1.0, 'end')
-#             text_box.grid(column=1, row=3)
-#         compression_text.set('Compress')
-
-
 
 if __name__ == '__main__':
-
-    from controller import Controller
 
     obj = View()
     obj.setup_main_interface()
