63b975d3f886a00087ed87aefe258fd2d489115d
LouaiKB <loukb1998@gmail.com>
2021-03-15 00:36:22 +0100

    updating controller

--- controller.py
+++ controller.py
@@ -31,7 +31,6 @@ class Controller:
         extension = os.path.splitext(self.file)[1]
         
         if extension == '.fasta':
-            print('uessss')
             sequence = SeqIO.read(self.file, 'fasta').seq
             self.sequence = sequence
 
@@ -41,38 +40,55 @@ class Controller:
                 for character in line:
                     if character != '\n':
                         self.sequence = self.sequence + character
-    
-    def compression_process_without_bwt(self) -> dict:
+
+    def compression_process_without_bwt(self, sequence:str) -> dict:
         """[summary]
+
+        Args:
+            sequence (str): [description]
 
         Returns:
             dict: [description]
         """
         compression_results = {}
-        self.huffman_object = HuffmanCompression(self.sequence)
+        self.huffman_object = HuffmanCompression(sequence)
         huffman_tree = self.huffman_object.binary_tree_huffman()
         self.huffman_object.sequence_to_binary_transformation()
         self.huffman_object.binary_to_unicode()
 
-        compression_results['binary_tree_huffman'] = huffman_tree
-        compression_results['sequence_to_binary'] = self.huffman_object.sequence_to_binary
-        compression_results['binary_to_unicode'] = self.huffman_object.unicode_sequence
+        compression_results['encoder'] = HuffmanCompression.code_generator(huffman_tree)
+        compression_results['Step1: Huffman binary tree'] = huffman_tree.__str__()
+        compression_results['Step2: Sequence to binary transfomation'] = self.huffman_object.sequence_to_binary
+        compression_results['Step3: Binary to unicode transformation'] = self.huffman_object.unicode_sequence
 
         return compression_results
     
-    def decompression_process_without_bwt(self) -> dict:
+    def decompression_process_without_bwt(self, unicode_file, encoder, binary_co) -> dict:
         """[summary]
 
         Returns:
             dict: [description]
         """
-        decompression_results = {}
-        self.huffman_object = HuffmanCompression(self.sequence)
-        unicode_to_binary = self.huffman_object.unicode_to_binary()
-        binary_to_sequence = self.huffman_object.decompression_process()
+        unicode = ''
+        with open(unicode_file, 'r', encoding='utf8') as f:
+            line = f.read()
+            for char in line:
+                if char != '\n':
+                    unicode += char
 
-        decompression_results['unicode_to_binary'] = unicode_to_binary
-        decompression_results['binary_to_sequence'] = binary_to_sequence
+        decompression_results = {}
+
+        self.huffman_object = HuffmanCompression(self.sequence)
+        self.huffman_object.unicode_sequence = unicode
+        self.huffman_object.sequence_to_binary = binary_co
+  
+        unicode_to_binary = self.huffman_object.unicode_to_binary()
+        binary_to_sequence = self.huffman_object.decompression_process(binary_co, encoder)
+
+        decompression_results['Step1: the unicode'] = unicode
+        decompression_results['Step2: unicode to binary transformation'] = unicode_to_binary
+        decompression_results['Step3: binary to sequence'] = binary_to_sequence
+
         return decompression_results
 
     def bwt_encryption_step_by_step(self) -> tuple:
@@ -92,12 +108,32 @@ class Controller:
         Returns:
             tuple: [description]
         """
-        self.bwt_object = Bwt(self.sequence)
-        bwt_reconstruction_matrix = self.bwt_object.bwt_reconstruction_matrix(self.sequence)
-        bwt_decryption = self.bwt_object.bwt_reconstruction()
+        self.bwt_object = Bwt('')
+        self.bwt_object.sequence = self.sequence
+        bwt_reconstruction_matrix = self.bwt_object.yield_()
+        bwt_decryption = self.bwt_object.bwt_reconstruction(self.sequence)
         return (bwt_reconstruction_matrix, bwt_decryption)
+    
+    def full_compression(self) -> dict:
+        """[summary]
+
+        Returns:
+            dict: [description]
+        """
+        full_compression = {}
+        bwt_results = self.bwt_encryption_step_by_step()
+        full_compression['Step 1']
+        bwt_sequence = bwt_results[1]
+        compression_results = self.compression_process_without_bwt(bwt_sequence)
+        compression_results['bwt_sequence'] = bwt_results[1]
+        compression_results['bwt_matrix'] = bwt_results[0]
+        return compression_results
+
+    def full_decompression(self):
+        pass
 
 if __name__ == '__main__':
     obj = Controller("C:\\Users\\Louai KB\\OneDrive - Aix-Marseille Université\\SEMESTRE 2\\Algorithmique et structures des données\\Algorithmique-Project\\NC_009513.fasta")
     obj.get_sequence_from_file()
-    print(obj.bwt_decryption()[0])
+    k = obj.compression_process_without_bwt(obj.sequence)
+    print(k)
