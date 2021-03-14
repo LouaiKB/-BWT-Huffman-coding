c8e2d55204d0f5975bec86d421fc11cf3351eca3
LouaiKB <loukb1998@gmail.com>
2021-03-15 00:40:24 +0100

    adding main file

---
+++ main.py
@@ -0,0 +1,17 @@
+# coding ~utf-8
+"""
+
+Author: Louai KB
+
+"""
+
+
+from view import View
+
+
+if __name__ == '__main__':
+
+    main_object = View()
+    main_object.setup_main_interface()
+    main_object.create_buttons()
+    main_object.main_loop()
